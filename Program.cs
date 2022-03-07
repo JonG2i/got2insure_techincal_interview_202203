11b110b875c65865e3f95150013fa9497d27ee7d
Jon Davies <jon@got2insure.com>
2022-03-07 13:43:36 +0000

    Create interview project

---
+++ Program.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+using Newtonsoft.Json.Linq;
+using CsvHelper;
+
+namespace Got2Insure.Technical.Interview202203
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var inputFileName = args[0];
+            var outputFolder = args[1];
+            var batchSize = int.Parse(args[2]);
+
+            var inputText = File.ReadAllText(inputFileName);
+            var inputJson = JObject.Parse(inputText);
+            var batchNumber = 1;
+            foreach (var inputQuoteBatch in Batch(inputJson["Quotes"], batchSize))
+            {
+                batchNumber++;
+
+                var processedQuoteBatch = inputQuoteBatch.Select(quote => new
+                {
+                    FirstName = quote["FirstName"].Value<string>(),
+                    Surname = quote["Surname"].Value<string>(),
+                    YearsWithNoClaims = quote["YearsWithNoClaims"].Value<int>(),
+                    MotorPremium = quote["MotorPremium"].Value<decimal>(),
+                    NoClaimsDiscountProtectionPremium = quote["NoClaimsDiscountProtectionPremium"].Value<decimal>()
+                });
+                
+                var outputFileName = GetOutputFileName(inputFileName, outputFolder, batchNumber);
+                using var outputFile = File.OpenWrite(outputFileName);
+                using var textWriter = new StreamWriter(outputFile);
+                using var csvWriter = new CsvWriter(textWriter, CultureInfo.InvariantCulture);
+                csvWriter.WriteRecords(processedQuoteBatch);
+            }
+        }
+
+        private static IEnumerable<IEnumerable<T>> Batch<T>(IEnumerable<T> source, int batchSize)
+        {
+            var startIndex = 0;
+            for (int i = 0; i <= source.Count(); i += batchSize)
+            {
+                var batch = source.Skip(startIndex).Take(batchSize).ToArray();
+                yield return batch;
+                startIndex += batchSize;
+            }
+        }
+
+        private static string GetOutputFileName(string inputFileName, string outputFolder, int batchNumber)
+        {
+            var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(inputFileName);
+            var outputFileName = outputFolder + @"\" + fileNameWithoutExtension + "_" + batchNumber + ".csv";
+            return outputFileName;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Got2Insure Technical Interview - March 2022
+
+In this folder you will find a simple C# project which will run on the .NET Core 5 framework.
+
+The code is "correct" - as in, it will run if a few conditions (which are not given here) are met - but there are some shortcomings. Please review the code in `Program.cs` before the interview, with consideration for the following:
+
+* Readability
+* Maintainability
+* Function
+* Performance
+
+In the interview we will review the code in your preference of Visual Studio 2019 or Visual Studio Code. If you have time, please ensure you can build the code before the interview. It is not necessary to fix any runtime exceptions you may encounter.
